516be847733cb952c3a9da319bd1a324727b559f
izzathabib <izzathabib680@gmail.com>
2024-12-01 19:46:25 +0800

    Sidebar initial implimentation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,50 +9,54 @@
     @vite(['resources/css/sidebarStyle.css'])
   </head>
   <body>
-        <aside id="sidebar">
-            <div class="d-flex">
-                <div class="sidebar-logo">
-                    <a href="#">CoffeeHourDay</a>
-                </div>
-                <button id="toggle-btn" type="button">
-                    <i class="lni lni-menu-hamburger-1"></i>
-                </button>
-            </div>    
-            <ul class="sidebar-nav">
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-home-2"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-file-pencil"></i>
-                        <span>Registration</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-calendar-days"></i>
-                        <span>Book Slot</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-user-multiple-4"></i>
-                        <span>Team Management</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-comment-1-share"></i>
-                        <span>Feedback</span>
-                    </a>
-                </li>
-            </ul>
-        </aside> 
-        
-        @yield('content')
+    <aside id="sidebar">
+        <!-- Sidebar logo -->
+        <div class="d-flex">
+            <div class="sidebar-logo">
+                <a href="#">CoffeeHourDay</a>
+            </div>
+            <button id="toggle-btn" type="button">
+                <i class="lni lni-menu-hamburger-1"></i>
+            </button>
+        </div>
+        <!-- / -->
+        <!-- Sidebar nav menu -->
+        <ul class="sidebar-nav">
+          <a href="#" class="sidebar-link">
+            <li class="sidebar-item">
+              <i class="lni lni-home-2"></i>
+              <span>Dashboard</span>
+            </li>
+          </a>
+          <a href="#" class="sidebar-link">
+            <li class="sidebar-item">
+              <i class="lni lni-file-pencil"></i>
+              <span>Registration</span>
+            </li>
+          </a>
+          <a href="#" class="sidebar-link">
+            <li class="sidebar-item">
+              <i class="lni lni-calendar-days"></i>
+              <span>Book Slot</span>
+            </li>
+          </a>
+          <a href="#" class="sidebar-link">
+            <li class="sidebar-item">
+              <i class="lni lni-user-multiple-4"></i>
+              <span>Team Management</span>
+            </li>
+          </a>
+          <a href="#" class="sidebar-link">
+            <li class="sidebar-item">
+              <i class="lni lni-comment-1-share"></i>
+              <span>Feedback</span>
+            </li>
+          </a>
+        </ul>
+        <!-- / -->
+    </aside> 
+    
+    @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
