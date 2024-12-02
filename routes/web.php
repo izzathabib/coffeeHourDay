c835373c2d0f1b682ba7ff477158bb4050adcac3
izzathabib <izzathabib680@gmail.com>
2024-12-02 10:51:50 +0800

    Setup feedbackForm page

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::view('coffee/dashboard', 'dashboard')->name('dashboard');
+Route::view('coffee/feedbackForm', 'feedback/feedbackForm')->name('feedbackForm');
