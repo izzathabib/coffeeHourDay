80c592ac0181eb0b3f3a5ba0f4dbb20e1c352843
izzathabib <izzathabib680@gmail.com>
2024-12-04 18:42:53 +0800

    Updating feedback module

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     @yield('styles')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.lineicons.com/5.0/lineicons.css" rel="stylesheet" />
-    @vite(['resources/css/sidebarStyle.css'])
+    @vite(['resources/css/sidebarStyle.css', 'resources/css/viewFeedbackStyle.css'])
   </head>
   <body>
     <div class="d-flex">
@@ -51,7 +51,7 @@
             </a>
           </li>
           <!-- Feedback dropdown -->
-          <li class="{{ Route::is('feedbackForm') ? 'active-word' : '' }}">
+          <li class="{{ Route::is(['feedbackForm', 'viewFeedback']) ? 'active-word' : '' }}">
             <a href="#feedbackSubmenu" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle sidebar-item">
               <i class="lni lni-comment-1-share"></i>
               <span>Feedback</span>
@@ -62,8 +62,8 @@
                   <span class="dropdown-word"><svg class="dropdown-svg" fill="#ff0088" width="64px" height="64px" viewBox="-20 -20 60.00 60.00" xmlns="http://www.w3.org/2000/svg" stroke="#ff0088" stroke-width="2"><g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(10,10), scale(0)"><rect x="-20" y="-20" width="60.00" height="60.00" rx="30" fill="#ffffff" strokewidth="0"></rect></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.16"></g><g id="SVGRepo_iconCarrier"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"></path></g></svg>Form</span>
                 </a> 
               </li>
-              <li>
-                <a href="#" class="sidebar-item">
+              <li class="{{ Route::is('viewFeedback') ? 'active' : '' }}">
+                <a href="{{ route('viewFeedback')}}" class="sidebar-item">
                   <span class="dropdown-word"><svg class="dropdown-svg2" fill="#59ff00" width="64px" height="64px" viewBox="-20 -20 60.00 60.00" xmlns="http://www.w3.org/2000/svg" stroke="#59ff00" stroke-width="2"><g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(10,10), scale(0)"><rect x="-20" y="-20" width="60.00" height="60.00" rx="30" fill="#ffffff" strokewidth="0"></rect></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.16"></g><g id="SVGRepo_iconCarrier"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"></path></g></svg>View Feedback</span>
                 </a>
               </li>
@@ -73,7 +73,7 @@
         </ul>
         <!-- / -->
     </aside> 
-    <div class="container">
+    <div class="container-fluid my-5">
       @yield('content')
     </div>
     </div>
